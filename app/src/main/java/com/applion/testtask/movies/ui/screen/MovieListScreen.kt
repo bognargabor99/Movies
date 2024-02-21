bcab1ae1c966e6b9bb943bd25e901bf18a383d5d
Gabor Bela Bognar <gaborbognar99@sch.bme.hu>
2024-02-21 23:35:15 +0100

    Better UI for Movie list

--- app/src/main/java/com/applion/testtask/movies/ui/screen/MovieListScreen.kt
+++ app/src/main/java/com/applion/testtask/movies/ui/screen/MovieListScreen.kt
@@ -1,34 +1,28 @@
 package com.applion.testtask.movies.ui.screen
 
-import android.widget.RatingBar
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,6 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -65,8 +60,8 @@ fun MovieListScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieListTopAppBar() {
-    TopAppBar(
-        title = { Text(text = stringResource(R.string.movies)) },
+    CenterAlignedTopAppBar(
+        title = { Text(text = stringResource(R.string.movies), fontWeight = FontWeight.Bold) },
     )
 }
 
@@ -75,15 +70,11 @@ fun MovieRowCard(
     movie: MovieResult
 ) {
     Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        ),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
         shape = RoundedCornerShape(10.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 5.dp,
-        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp,),
         modifier = Modifier
-            .padding(5.dp)
+            .padding(8.dp)
             .fillMaxWidth()
             .wrapContentHeight()
     ) {
@@ -108,17 +99,18 @@ fun MovieRowCard(
             ) {
                 Text(
                     text = movie.title,
-                    style = MaterialTheme.typography.titleLarge
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
                 )
                 Row(
                     horizontalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    Icon(imageVector = Icons.Filled.Star, contentDescription = null, tint = Color.Yellow)
-                    Text(movie.voteAverage.toString(), color = Color.Yellow)
-                    Icon(imageVector = Icons.Filled.Star, contentDescription = null, tint = Color.Red)
-                    Text(movie.voteAverage.toString(), color = Color.Red)
+                    Icon(imageVector = Icons.Filled.Star, contentDescription = null, tint = Color(180, 165, 0))
+                    Text(movie.voteAverage.toString(), color = Color(180, 165, 0))
+                    Icon(imageVector = Icons.Filled.Favorite, contentDescription = null, tint = Color.Red)
+                    Text(movie.voteCount.toString(), color = Color.Red)
                 }
-                Text(text = movie.overview, maxLines = 3)
+                Text(text = movie.overview, maxLines = 3, color = Color.Gray)
             }
         }
     }
@@ -161,7 +153,9 @@ fun LoadingScreen() {
 
 @Composable
 fun MovieList(movies: List<MovieResult>) {
-    LazyColumn {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ) {
         items(movies.size) {
             MovieRowCard(movie = movies[it])
         }
